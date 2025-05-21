d16bc8604b5ba0df215b190d5c540c1368738531
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2025-05-21 08:27:24 +0700

    Initial Frontend

---
+++ src/main/java/com/seyha/spring_boot_graphql/Controller/BookController.java
@@ -0,0 +1,30 @@
+package com.seyha.spring_boot_graphql.Controller;
+
+import com.seyha.spring_boot_graphql.Author;
+import com.seyha.spring_boot_graphql.Book;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class BookController {
+
+    @QueryMapping
+    public List<Book> books() {
+        return Book.books;
+    }
+
+    @QueryMapping
+    public Optional<Book> bookById(@Argument Integer id) {
+        return Book.getBookById(id);
+    }
+
+    @SchemaMapping
+    public Optional<Author> author(Book book) {
+        return Author.getAuthorById(book.authorId());
+    }
+}
